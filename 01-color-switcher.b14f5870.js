0dd2855f8c8e1f3e319701e919793649d8a33ca7
MartyniukOleg91 <111987629+MartyniukOleg91@users.noreply.github.com>
2023-03-09 07:24:36 +0000

    Deploying to gh-pages from @ MartyniukOleg91/goit-js-hw-09@fa129bd3a47d2ef0d865ef60f034f30b4f6e6c37 🚀

--- 01-color-switcher.b14f5870.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let l=null;function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.disabled=!1,e.disabled=!0,t.addEventListener("click",(function(){console.log("Call onBtnStartClick every 500 ms"),l=setInterval(n,500),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(l),console.log("Interval has stopped!")}));
-//# sourceMappingURL=01-color-switcher.b14f5870.js.map
